43eac8bb1917887fa5c6df92d52e349477f33cb0
이종진 <jin1122877@naver.com>
2025-05-27 16:28:15 +0900

    Fix: Correct article detail function name in article.routes.js

--- src/routes/article.routes.js
+++ src/routes/article.routes.js
@@ -10,7 +10,7 @@ router
 
 router
   .route('/:id')
-  .get(articleController.getArticleById)
+  .get(articleController.getArticle)
   .patch(validateArticle, articleController.updateArticle)
   .delete(articleController.deleteArticle);
 
