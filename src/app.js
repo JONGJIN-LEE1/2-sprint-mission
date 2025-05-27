557197defd92a5dbf7014462a979594b8bacc32a
이종진 <jin1122877@naver.com>
2025-05-27 17:23:55 +0900

    feat: Configure static file serving and add image upload route in app.js

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const productRoutes = require('./routes/product.routes');
 const uploadRoutes = require('./routes/upload.routes');
 const articleRoutes = require('./routes/article.routes');
 const commentRoutes = require('./routes/comment.routes');
+const imageRoutes = require('./routes/image.routes');
 const path = require('path');
 
 const app = express();
@@ -17,17 +18,26 @@ app.use(cors({
 }));
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // 라우트
 app.use('/products', productRoutes);
 app.use('/upload', uploadRoutes);
 app.use('/articles', articleRoutes);
 app.use('/comments', commentRoutes);
-
+app.use('/api/upload', imageRoutes);
 // 정적 파일 제공
-app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+
 
 // 에러 처리
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use((err, req, res, next) => {
+    console.error(err.stack); 
+    
+    res.status(err.statusCode || 500).json({
+        message: err.message || '서버 내부 에러 발생!',
+        
+    });
+});
 
 module.exports = app;
