1b3e4f7dd0782712b9f9abd59a2c2b6fa25ce61e
JONGJIN-LEE1 <jin1122877@naver.com>
2025-08-26 18:47:39 +0900

    feat: 테스트 순차 실행으로 데드락 방지

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,34 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/src', '<rootDir>/tests'],
-  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'],
   transform: {
-    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          esModuleInterop: true,
+          allowSyntheticDefaultImports: true,
+          moduleResolution: 'node',
+          resolveJsonModule: true,
+          isolatedModules: true,
+          noEmit: true,
+          strict: true,
+          skipLibCheck: true,
+          forceConsistentCasingInFileNames: true,
+          module: 'commonjs',
+          target: 'ES2019',
+        },
+      },
+    ],
   },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^../utils/(.*)$': '<rootDir>/tests/utils/$1',
+    '^../../src/(.*)$': '<rootDir>/src/$1',
+  },
+  moduleDirectories: ['node_modules', 'src'],
   collectCoverageFrom: [
     'src/**/*.ts',
     '!src/**/*.d.ts',
@@ -16,8 +39,11 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'html'],
   setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
   testTimeout: 10000,
+  maxWorkers: 1, // 테스트를 순차적으로 실행하여 데드락 방지
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+    },
+  },
 };
