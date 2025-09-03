e53dd98646795c83b64567feca1313e6ab20b070
JONGJIN-LEE1 <jin1122877@naver.com>
2025-09-03 17:29:21 +0900

    feat: constants 수정/추가

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -8,5 +8,7 @@ export const JWT_ACCESS_TOKEN_SECRET = process.env.JWT_ACCESS_TOKEN_SECRET || ''
 export const JWT_REFRESH_TOKEN_SECRET = process.env.JWT_REFRESH_TOKEN_SECRET || '';
 export const NODE_ENV = process.env.NODE_ENV || 'development';
 export const PORT = process.env.PORT || 3000;
-export const PUBLIC_PATH = './public';
-export const STATIC_PATH = '/public';
+export const PUBLIC_PATH =
+  process.env.NODE_ENV === 'production' ? process.env.AWS_S3_BUCKET_NAME! : 'public';
+export const STATIC_PATH = 'static';
+export const IS_PRODUCTION = process.env.NODE_ENV === 'production';
